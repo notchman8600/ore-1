16df794ec1fa25e4360e43138565bc89e7398bf7
notch_man <miyake.kentaro0815@mokkuriko.com>
2023-12-11 01:17:36 +0900

    エラー処理の追加

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ import json
 
 # Third Party Library
 from asgiref.typing import ASGIReceiveCallable, ASGISendCallable, HTTPRequestEvent, HTTPScope, Scope
+from jinja2 import Environment, FileSystemLoader
 from ore_logger import logger
 from router.http_router import dispatch_http_event
-from jinja2 import Environment, FileSystemLoader
+from template import render_template
+
 
 async def app(scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
     # JSONデータを準備
@@ -21,10 +23,12 @@ async def app(scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
                 await dispatch_http_event(scope, receive, send)
             except Exception as e:
                 logger.error(f"error has occured in http event: {e}")
+
                 pass
     except Exception as e:
-        logger.error(f"unexpected error has occured: {e}")
         # ここでの例外はイベント起動に関わる例外のみで、アプリケーションレベルの例外はdispatcherのハンドラで全て処理を行う
+        # ここでトラップされるエラーがあるのか？
+        logger.error(f"unexpected error has occured: {e}")
         pass
 
     # # レスポンスの開始を送信
--- app/router/http_router.py
+++ app/router/http_router.py
@@ -21,19 +21,34 @@ def http_router(scope: HTTPScope, receive: ASGIReceiveCallable) -> tuple[bytes, 
         # assert event["type"] == "http.request"
         # request_body = json.loads(event["body"].decode())
         # return {"message": f"hello, {request_body['name']}"}
-
     else:
         return b"application/json", json.dumps({"message": "Not Found"}).encode()
 
 
 async def dispatch_http_event(scope: HTTPScope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
-    content_type, encoded_res = http_router(scope, receive)
-    # レスポンスヘッダ
-    headers = [
-        (b"content-type", content_type),
-        (b"content-length", str(len(encoded_res)).encode(encoding="utf-8")),
-    ]
-    await send({"type": "http.response.start", "status": 200, "headers": headers})
-    await send({"type": "http.response.body", "body": encoded_res})
-
+    # dispatch errorを全体的に囲むことで白い画面を出さないようにする
+    # 本当はAPIコールかHTMLコールかによって例外処理を考える必要がある
+    # APIコールであればエラーオブジェクトのJSON、HTMLであればエラーページをレンダリングする必要がある
+    try:
+        content_type, encoded_res = http_router(scope, receive)
+        # レスポンスヘッダ
+        headers = [
+            (b"content-type", content_type),
+            (b"content-length", str(len(encoded_res)).encode(encoding="utf-8")),
+        ]
+        await send({"type": "http.response.start", "status": 200, "headers": headers})
+        await send({"type": "http.response.body", "body": encoded_res})
+    except Exception as e:
+        # HTTPレスポンスの準備
+        html_content = render_template("error.jinja2.html", {"message": "hello, I am variable value."})
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 500,
+                "headers": [
+                    [b"content-type", b"text/html"],
+                ],
+            }
+        )
+        await send({"type": "http.response.body", "body": html_content.encode("utf-8")})
     return
